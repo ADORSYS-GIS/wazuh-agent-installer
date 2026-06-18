91c1f37067b949c6855e3ab6cacc41f95b0e6a44
chojuninengu <chojuninengu@gmail.com>
2026-06-18 17:54:43 +0100

    chore: reduce setup scripts to absolute bare minimum one-liners

--- setup-agent.ps1
+++ setup-agent.ps1
@@ -1,12 +1,1 @@
-param(
-    [string]$RepoUrl = "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent",
-    [string]$Ref = $(
-        if ($env:WAZUH_AGENT_REPO_REF) { $env:WAZUH_AGENT_REPO_REF } else { 'fix/error-message-helper-function' }
-    )
-)
-
-$env:WAZUH_AGENT_REPO_REF = $Ref
-$env:WAZUH_AGENT_STATUS_REPO_REF = "user-main"
-$env:INSTALL_CERT_AUTH = "TRUE"
-
-Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/refs/heads/${Ref}/scripts/setup-agent.ps1" -UseBasicParsing).Content
+Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/refs/heads/fix/error-message-helper-function/scripts/setup-agent.ps1" -UseBasicParsing).Content
--- setup-agent.sh
+++ setup-agent.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 set -eu
 
-export WAZUH_AGENT_REPO_REF="${WAZUH_AGENT_REPO_REF:-fix/error-message-helper-function}"
-export WAZUH_AGENT_STATUS_REPO_REF="user-main"
-export INSTALL_CERT_AUTH="TRUE"
-
-curl -s "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/refs/heads/${WAZUH_AGENT_REPO_REF}/scripts/setup-agent.sh" | bash -s -- "$@"
+curl -s "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/refs/heads/fix/error-message-helper-function/scripts/setup-agent.sh" | bash -s -- "$@"
