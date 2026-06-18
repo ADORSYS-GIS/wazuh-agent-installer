7dc3ad5aee7aa7d49f8af1126353394023fd0e3d
chojuninengu <chojuninengu@gmail.com>
2026-06-18 16:22:47 +0100

    chore: bump wazuh agent version to 4.14.4

--- setup-agent.ps1
+++ setup-agent.ps1
@@ -5,7 +5,7 @@ param(
     )
 )
 
-$env:WAZUH_AGENT_VERSION = "4.14.1-1"
+$env:WAZUH_AGENT_VERSION = "4.14.4-1"
 $env:WAZUH_AGENT_STATUS_VERSION = "v0.5.0-rc.12"
 $env:WAZUH_AGENT_STATUS_REPO_REF = "v0.5.0-rc.12"
 $env:APP_VERSION = "v0.5.0-rc.12"
