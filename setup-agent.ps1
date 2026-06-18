a69050c7661fdbdcc2c65475891e5e0df7645552
chojuninengu <chojuninengu@gmail.com>
2026-06-18 16:22:13 +0100

    chore: bump agent status version to v0.5.0-rc.12

--- setup-agent.ps1
+++ setup-agent.ps1
@@ -6,9 +6,9 @@ param(
 )
 
 $env:WAZUH_AGENT_VERSION = "4.14.1-1"
-$env:WAZUH_AGENT_STATUS_VERSION = "v0.5.0-rc.11"
-$env:WAZUH_AGENT_STATUS_REPO_REF = "v0.5.0-rc.11"
-$env:APP_VERSION = "v0.5.0-rc.11"
+$env:WAZUH_AGENT_STATUS_VERSION = "v0.5.0-rc.12"
+$env:WAZUH_AGENT_STATUS_REPO_REF = "v0.5.0-rc.12"
+$env:APP_VERSION = "v0.5.0-rc.12"
 $ScriptRoot = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
 
 # Decide remote path depending on OS
