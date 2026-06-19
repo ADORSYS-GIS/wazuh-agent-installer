9ee06bf14a36ee82808062cd2f5368a0bd13568c
chojuninengu <chojuninengu@gmail.com>
2026-06-19 11:58:07 +0100

    chore: point setup wrappers to the main branch

--- setup-agent.ps1
+++ setup-agent.ps1
@@ -1,1 +1,1 @@
-Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/refs/heads/fix/error-message-helper-function/scripts/setup-agent.ps1" -UseBasicParsing).Content
+Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/refs/heads/main/scripts/setup-agent.ps1" -UseBasicParsing).Content
--- setup-agent.sh
+++ setup-agent.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 
-curl -s "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/refs/heads/fix/error-message-helper-function/scripts/setup-agent.sh" | bash -s -- "$@"
+curl -s "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/refs/heads/main/scripts/setup-agent.sh" | bash -s -- "$@"
