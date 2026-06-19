4374b6956fbbeb27d9ca52b93fa633ed88cd44ac
chojuninengu <chojuninengu@gmail.com>
2026-06-19 15:14:09 +0100

    chore: point setup wrappers to the v1.8.1-rc.2 tag

--- setup-agent.ps1
+++ setup-agent.ps1
@@ -1,1 +1,1 @@
-Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/refs/heads/chore/release-v1.8.1/scripts/setup-agent.ps1" -UseBasicParsing).Content
+Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/refs/tags/v1.8.1-rc.2/scripts/setup-agent.ps1" -UseBasicParsing).Content
