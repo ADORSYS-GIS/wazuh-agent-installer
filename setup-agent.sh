d9206d25189eab6276c512801035cbaac3e23acb
chojuninengu <chojuninengu@gmail.com>
2026-07-01 15:18:21 +0100

    chore(scripts): track develop branch of wazuh-agent for stable windows status monitor

--- setup-agent.sh
+++ setup-agent.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 
-curl -s "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/refs/tags/v1.8.1/scripts/setup-agent.sh" | bash -s -- "$@"
+curl -s "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/develop/scripts/setup-agent.sh" | bash -s -- "$@"
