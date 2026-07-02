de2b143efe4d914127ce77beaee3e818dd5b1a6f
chojuninengu <chojuninengu@gmail.com>
2026-07-02 17:04:07 +0100

    chore(scripts): track wazuh-agent v1.8.0 tag

--- setup-agent.ps1
+++ setup-agent.ps1
@@ -1,1 +1,1 @@
-Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/develop/scripts/setup-agent.ps1" -UseBasicParsing).Content
+Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/refs/tags/v1.8.0/scripts/setup-agent.ps1" -UseBasicParsing).Content
