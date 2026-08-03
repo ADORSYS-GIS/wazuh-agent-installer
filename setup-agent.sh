0a6d1766c35e82a15d6f55808be9181f105c403f
chojuninengu <chojuninengu@gmail.com>
2026-08-03 14:02:22 +0100

    fix: add missing shebang in setup-agent.sh

--- setup-agent.sh
+++ setup-agent.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+set -eu
+
 # Default to the develop branch so inner scripts resolve correctly;
 # users can override by setting WAZUH_AGENT_REPO_REF themselves.
 export WAZUH_AGENT_REPO_REF="${WAZUH_AGENT_REPO_REF:-develop}"
