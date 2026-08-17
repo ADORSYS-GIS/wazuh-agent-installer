360231087849ff3e8a03892270396d1ebd99bba6
chojuninengu <chojuninengu@gmail.com>
2026-08-17 09:07:10 +0100

    fix(agent): update default WAZUH_AGENT_REPO_REF to v1.8.1

--- setup-agent.ps1
+++ setup-agent.ps1
@@ -1,4 +1,4 @@
-$env:WAZUH_AGENT_REPO_REF = if ($env:WAZUH_AGENT_REPO_REF) { $env:WAZUH_AGENT_REPO_REF } else { "v1.8.1-rc.1" }
+$env:WAZUH_AGENT_REPO_REF = if ($env:WAZUH_AGENT_REPO_REF) { $env:WAZUH_AGENT_REPO_REF } else { "v1.8.1" }
 $tmpScript = Join-Path $env:TEMP "wazuh-setup-agent-real_$((Get-Date).Ticks).ps1"
 try {
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/$env:WAZUH_AGENT_REPO_REF/scripts/setup-agent.ps1" -OutFile $tmpScript -UseBasicParsing
